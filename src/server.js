671e49a7bb4aaabc216ac47dda2f9c3892c689b7
lqdchrm <lqdchrm-code@yahoo.de>
2022-01-26 21:09:11 +0100

    server: some logging and disconnect handling

--- src/server.js
+++ src/server.js
@@ -16,19 +16,32 @@ const io = new ioLib.Server(httpServer);
 
 // socket.io setup
 io.on('connection', (socket) => {
+
+    // user connected
     console.log(`[IO] <${socket.id}> a user connected`);
+    let msg = `User <${socket.id}> connected`;
+    socket.broadcast.emit('chat message', { from: socket.id, msg });
 
     // log all messages
     socket.onAny((message, ...args) => {
         console.log(`[IO] <${socket.id}> Received: ${message}`, ...args);
     });
 
+    // handle disconnect
+    socket.on('disconnect', (reason) => {
+        console.log(`[IO] <${socket.id}> a user disconnected: ${reason}`);
+        let msg = `User <${socket.id}> disconnected: ${reason}`;
+        socket.broadcast.emit('chat message', { from: socket.id, msg });
+    });
+
     // handle messages
     socket.on('chat message', (msg) => {
-        io.emit('chat message', {from: socket.id, msg});
-      });
+        io.emit('chat message', { from: socket.id, msg });
+    });
 });
 
-httpServer.listen(3000, () => {
-    console.log('[SERVER] listening on *:3000');
+// start server
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => {
+    console.log(`[SERVER] listening on *:${PORT}`);
 })
