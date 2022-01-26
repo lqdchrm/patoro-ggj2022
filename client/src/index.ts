122895becb582cf163b2038c8d381030ce2950fb
Patrick Kranz <patrick-kranz@live.de>
2022-01-26 08:23:18 +0100

    move will now work turn based...

--- client/src/index.ts
+++ client/src/index.ts
@@ -4,59 +4,183 @@ import { Resources } from "./resources";
 import ioclient, { io } from "socket.io-client";
 import { Keys } from "excalibur/build/dist/Input";
 
-const socket = ioclient();
 
-type join = {
+
+type UserData = {
   room: string;
   user: string;
 }
 
-type updateData = {
+type Join = {
+
+}
+
+type Move = {
 
   x: number;
   y: number;
-} & join;
+};
 
 
-type playerLockup = {
-  [key: string]: Player;
+type Handlers = {
+  [S in CommunicationEvents]?: EventHandler<S>[]
+}
+
+
+type CommunicationEvents = 'AddPlayer' | 'RemovePlayer' | 'NewTurn';
+type EventArguments<E extends CommunicationEvents> =
+  E extends 'AddPlayer' ? { player: Player }
+  : E extends 'RemovePlayer' ? { player: Player }
+  : E extends 'NewTurn' ? {
+    moves: ({ player: Player } & Move)[]
+  }
+  : never;
+type EventHandler<E extends CommunicationEvents> = (event: EventArguments<E>) => void;
+
+class Communication {
+  private readonly socket = ioclient();
+  private readonly userData: UserData
+  private readonly players: Record<string, Player> = {};
+  private moves: Record<string, Move & { player: Player }> = {};
+  private readonly userPlayer: Player;
+  private readonly handlers: Handlers = {};
+
+  private readonly createPlayer: (userData: UserData) => Player;
+
+  constructor(userData: UserData, userPlayer: Player, createPlayer: (userData: UserData) => Player) {
+    this.userData = userData;
+    this.userPlayer = userPlayer;
+    this.createPlayer = createPlayer;
+    this.socket.on('join', data => this.handleJoin(data))
+    this.socket.on('left', data => this.handleLeve(data))
+    this.socket.on('move', data => this.handleMove(data))
+    this.send('join', userData);
+  }
+  private handleJoin(data: Join & UserData) {
+    console.log("recived Join", data);
+    console.log("recived Join log me", this);
+    if (!this.players[data.user] && data.user != this.userData.user) {
+      console.log(`Player ${data.user} joind`)
+      const newPlayer = this.createPlayer(data);
+      this.players[data.user] = newPlayer;
+      this.socket.emit('join', this.userData);
+      const handlers = this.handlers.AddPlayer;
+      if (handlers) {
+        for (let index = 0; index < handlers.length; index++) {
+          const handler = handlers[index];
+          handler({ player: newPlayer });
+        }
+      }
+    }
+  }
+
+  private handleLeve(data: Join & UserData) {
+    console.log("recived left", data);
+    if (this.players[data.user] && data.user != this.userData.user) {
+
+      const oldPlayer = this.players[data.user];
+
+      const handlers = this.handlers.RemovePlayer;
+      if (handlers) {
+        for (let index = 0; index < handlers.length; index++) {
+          const handler = handlers[index];
+          handler({ player: oldPlayer });
+        }
+      }
+    }
+  }
+
+  private handleMove(data: Move & UserData) {
+    if (!this.moves[data.user] && data.user != this.userData.user) {
+      console.log(`Player ${data.user} moved`)
+
+      this.moves[data.user] = { ...data, player: this.players[data.user] };
+
+      // test if we have all moves including our own
+      this.CheckAllMoved();
+
+
+    }
+  }
+
+  private CheckAllMoved() {
+    if (Object.keys(this.moves).length == Object.keys(this.players).length + 1) {
+      const handlers = this.handlers.NewTurn;
+      if (handlers) {
+        console.log('All Moves are present');
+        for (let index = 0; index < handlers.length; index++) {
+          const handler = handlers[index];
+          const moveArray = Object.keys(this.moves).map(k => this.moves[k]);
+          handler({ moves: moveArray });
+          this.moves = {};
+        }
+      }
+    }
+  }
+
+  public SubmitMove(data: Move) {
+    console.log('try submit move Move');
+    if (!this.moves[this.userData.user]) {
+      console.log('Submitting Move');
+      this.moves[this.userData.user] = { ...data, player: this.userPlayer };
+      this.send('move', data);
+      this.CheckAllMoved();
+
+    }
+  }
+
+  public register<E extends CommunicationEvents>(event: E, handler: EventHandler<E>) {
+    if (!this.handlers[event]) {
+      this.handlers[event] = [];
+    }
+    const h = this.handlers[event] as EventHandler<E>[];
+    h.push(handler);
+  }
+
+  private send<T extends object>(message: string, data: T) {
+    this.socket.emit(message, { ...this.userData, ...data });
+  }
+
+
+
+
+
 }
 
 class Game extends Engine {
   initialize() {
 
-    const players: playerLockup = {};
 
     const userName = Math.random().toString();
 
     const player = new Player(userName);
     this.add(player);
 
-    const joinData: join = {
+    const joinData: UserData = {
       user: userName,
       room: location.search ?? 'myRoom'
     };
-    socket.emit('join', joinData);
+    const communication = new Communication(joinData, player, data => new Player(data.user));
 
-    socket.on("join", async (data: join) => {
-      console.log("join", data)
-      if (!players[data.user] && data.user != userName) {
-
-        const newPlayer = new Player(data.user);
-        this.add(newPlayer);
-        players[data.user] = newPlayer;
-        socket.emit('join', joinData);
-      }
-    });
-
-    socket.on('update', (data: updateData) => {
-      const player = players[data.user];
-
-      if (player) {
-        player.pos.x = data.x;
-        player.pos.y = data.y;
-      }
+    communication.register('AddPlayer', data => {
+      this.add(data.player);
     })
+    communication.register('RemovePlayer', data => {
+      console.log('Remove player')
+      this.remove(data.player)
+    })
+
+
+    communication.register('NewTurn', data => {
+
+      for (let index = 0; index < data.moves.length; index++) {
+        const move = data.moves[index];
+        move.player.pos.x = move.x;
+        move.player.pos.y = move.y;
+      }
+
+    })
+
 
     const loader = new Loader();
     loader.addResource(Resources.Sword);
@@ -66,32 +190,30 @@ class Game extends Engine {
 
     this.on('preupdate', e => {
       const keys = this.input.keyboard.getKeys();
-      const speed = 0.5;
+      const speed = 20;
       let didMove = false;
+      const targetPost = { x: player.pos.x, y: player.pos.y };
       if (keys.includes("ArrowRight")) {
-        player.pos.x = player.pos.x + e.delta * speed;
+        targetPost.x = player.pos.x + speed;
         didMove = true;
       }
       if (keys.includes("ArrowLeft")) {
-        player.pos.x = player.pos.x - e.delta * speed;
+        targetPost.x = player.pos.x - speed;
         didMove = true;
       }
       if (keys.includes("ArrowDown")) {
-        player.pos.y = player.pos.y + e.delta * speed;
+        targetPost.y = player.pos.y + speed;
         didMove = true;
       }
       if (keys.includes("ArrowUp")) {
-        player.pos.y = player.pos.y - e.delta * speed;
+        targetPost.y = player.pos.y - speed;
         didMove = true;
       }
       if (didMove) {
-        const data: updateData = {
-          ...joinData,
+        communication.SubmitMove(
+          targetPost
+        )
 
-          x: player.pos.x,
-          y: player.pos.y
-        };
-        socket.emit('update', data)
       }
     })
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -15,16 +15,12 @@ if (env == "development") {
 const PORT = process.env.PORT || 5000
 
 
-type join = {
+
+type UserData = {
   room: string;
   user: string;
 }
 
-type updateData = {
-
-  x: number;
-  y: number;
-} & join;
 
 
 const app = express();
@@ -34,23 +30,27 @@ var httpServer = new http.Server(app);
 const io = new socket.Server(httpServer);
 
 io.on('connection', socket => {
-  socket.on('join', (data: join) => {
-    console.log(`Join ${data.user} in ${data.room}`)
-
-    socket.to(data.room).emit('join', data);
-    socket.join(data.room);
-
-
-  })
-
-  socket.on('update', (data: updateData) => {
-    socket.to(data.room).emit('update', data);
-    if (socket.rooms.has(data.room)) {
+  let room: string | undefined;
+  let user: string | undefined;
+  console.log('connected');
+  socket.onAny((message, data: UserData) => {
+    console.log(`Recived ${message} from ${data.user}`)
+    socket.to(data.room).emit(message, data);
+    // join the room if not already
+    if (!socket.rooms.has(data.room)) {
+      room = data.room;
+      user = data.user;
+      console.log(`Join ${data.user} in ${data.room}`)
       socket.join(data.room);
     }
   })
 
-
+  socket.on('disconnect', reason => {
+    console.log(`Recived disconect from ${user}`)
+    if (room) {
+      io.to(room).emit('left', { user: user });
+    }
+  })
 });
 
 
@@ -60,7 +60,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 
 
-// start our simple server up on localhost:3000
+// start our simple server up on localhost:PORT
 const server = httpServer
   .listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
 
