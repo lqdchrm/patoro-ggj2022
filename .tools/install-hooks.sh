d7a35593df28863ad3262ad86dcac699e1c05a92
lqdchrm <lqdchrm-code@yahoo.de>
2022-01-27 01:10:54 +0100

    fix for server hook

--- .tools/install-hooks.sh
+++ .tools/install-hooks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git config --local receive.denyCurrentBranch warn
 rm ./.git/hooks/post-receive
 ln -s ../../.tools/post-receive ./.git/hooks/post-receive
 chmod +x ./.tools/post-receive
