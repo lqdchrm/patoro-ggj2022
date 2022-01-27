09ed73078028a8371120fea081a1d84d09558a5c
lqdchrm <lqdchrm-code@yahoo.de>
2022-01-27 01:07:31 +0100

    fix for post-receive hook

--- .tools/install-hooks.sh
+++ .tools/install-hooks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm ./.git/hooks/post-receive
-ln -s ./.tools/post-receive ./.git/hooks/post-receive
+ln -s ../../.tools/post-receive ./.git/hooks/post-receive
 chmod +x ./.tools/post-receive
